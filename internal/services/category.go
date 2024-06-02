a4fc1c6f35cfa4bab30a0ed231460429dd473631
Vinicius Italo da Cruz <viniciusidacruz@hotmail.com>
2024-06-02 01:52:46 -0300

    feat: :sparkles: Service Category
    Create service for course category

---
+++ internal/services/category.go
@@ -0,0 +1,37 @@
+package services
+
+import (
+	"context"
+
+	"github.com/viniciusidacruz/grpc-http2/internal/database"
+	"github.com/viniciusidacruz/grpc-http2/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: categoryDB,
+	}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+
+	if err != nil {
+		return nil, err
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return &pb.CategoryResponse{
+		Category: categoryResponse,
+	}, nil
+}
